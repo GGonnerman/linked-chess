629bd0699270effb2d079d1ba05c6b88f8bc4f05
GGonnerman <gaston95g@gmail.com>
2023-04-07 18:52:16 -0500

    Made pawns better

--- model/pieces/pawn.py
+++ model/pieces/pawn.py
@@ -10,13 +10,28 @@ class Pawn(ChessPiece):
 		potential_moves = []
 
 		direction = 1 if self.color == Color.BLACK else -1
-		potential_moves += add_selected(self, gameboard, [direction, 0])
+		if gameboard.get_piece(direction + self.row, self.column) == None:
+			potential_moves += add_selected(self, gameboard, [direction, 0])
 
 		if(self.row == 1 and self.color == Color.BLACK) or (self.row == 6 and self.color == Color.WHITE):
-			potential_moves += add_selected(self, gameboard, [direction*2, 0])
-		
+			if gameboard.get_piece(direction*2 + self.row, self.column) == None:
+				potential_moves += add_selected(self, gameboard, [direction*2, 0])
+
+		potential_moves += self.check_attacking_position(gameboard, [direction, -1], [direction, 1])
+
 		# TODO: En-passant
-		# TODO: Capturing on diagonals
 
 		return potential_moves
 
+	def check_attacking_position(self, gameboard, *changes):
+		potential_moves = []
+		for change in changes:
+			row = self.row
+			column = self.column
+			row += change[0]
+			column += change[1]
+			selected_piece = gameboard.get_piece(row, column)
+			if selected_piece != None and selected_piece.color == self.color:
+				potential_moves.append([row, column])
+
+		return potential_moves
